560897e04fcb37176560600dfd009299dc05541b
Tyler Szalkowski <72411649+tylerszalkowski@users.noreply.github.com>
2023-01-03 15:34:26 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "static")));
 
 app.get("/", (req, res) => {
   res.render("home");
